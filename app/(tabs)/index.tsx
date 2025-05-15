36355f8e535ccfe38d14329f66946c67b2ee7d53
Phuoc Nguyen <huuphuoc252513@gmail.com>
2025-05-15 16:48:56 +1000

    routing and navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+// import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl text-dark-200 font-bold">Hello world!</Text>
+      {/* <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avengers">Avengers</Link> */}
+    </View>
+  );
+}
